720e3ff7ed874c9af5c52337a6a9b53a22c4467d
wangjiahao <wangjiahao@megvii.com>
2026-05-15 12:42:49 +0800

    add tinystories dataset

--- prepare_data.py
+++ prepare_data.py
@@ -6,70 +6,81 @@ from typing import List
 import numpy as np
 import requests
 import tiktoken
+from datasets import load_dataset
 
 
 @dataclass
 class PrepareConfig:
-    data_url: str = "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt"
-    data_dir: str = "./data/shakespeare_bpe"
-    train_ratio: float = 0.9
+    data_dir: str = "./data/tinystories_gpt2"
     dtype: np.dtype = np.uint16
     tokenizer_name: str = "gpt2"
+    num_proc: int = 20
 
 
-class ShakespearePreparer:
+class TinyStoriesPreparer:
     def __init__(self, cfg: PrepareConfig):
         self.data_dir = Path(cfg.data_dir)
-        self.data_url = cfg.data_url
-        self.train_ratio = cfg.train_ratio
         self.dtype = cfg.dtype
         self.tokenizer_name = cfg.tokenizer_name
+        self.num_proc = cfg.num_proc  # 控制预处理进程数
 
-        self.input_text = self.data_dir / "input.txt"
         self.train_bin = self.data_dir / "train.bin"
         self.val_bin = self.data_dir / "val.bin"
         self.meta_file = self.data_dir / "meta.pkl"
-        self.tokenizer = tiktoken.get_encoding(cfg.tokenizer_name)
+
+        self.enc = tiktoken.get_encoding(cfg.tokenizer_name)
 
     def prepare(self):
         self.data_dir.mkdir(parents=True, exist_ok=True)
 
-        text = self._load_text()
-        print(f"length of dataset in characters: {len(text)}")
-        split_idx = int(len(text) * self.train_ratio)
-        train_text, val_text = text[:split_idx:], text[split_idx:]
+        ds = load_dataset("roneneldan/TinyStories")
+        train_ds, val_ds = ds["train"], ds["validation"]
 
-        train_ids = self.tokenizer.encode_ordinary(train_text)
-        val_ids = self.tokenizer.encode_ordinary(val_text)
-        print(f"train has {len(train_ids)} tokens, val has {len(val_ids)} tokens")
+        def process(example):
+            ids = self.enc.encode_ordinary(example["text"])
+            return {"ids": ids, "len": len(ids)}
 
-        self._save_bin(self.train_bin, train_ids)
-        self._save_bin(self.val_bin, val_ids)
+        train_tokenized = train_ds.map(
+            process,
+            remove_columns=train_ds.column_names,  # 去除原本的文本，只保留转化后的token
+            desc="tokenizing train split",
+            num_proc=self.num_proc,
+        )
+        val_tokenized = val_ds.map(
+            process,
+            remove_columns=val_ds.column_names,
+            desc="tokenizing val split",
+            num_proc=self.num_proc,
+        )
+        train_total_tokens = sum(train_tokenized["len"])
+        val_total_tokens = sum(val_tokenized["len"])
+
+        self._write_memap(self.train_bin, train_tokenized, train_total_tokens)
+        self._write_memap(self.val_bin, val_tokenized, val_total_tokens)
+
+        print(f"train has {train_total_tokens} tokens")
+        print(f"val has {val_total_tokens} tokens")
+
         self._save_meta()
 
-    def _load_text(self):
-        if not self.input_text.exists():
-            self._download_text()
-        return self.input_text.read_text(encoding="utf-8")
-
-    def _download_text(self):
-        response = requests.get(self.data_url, timeout=30)
-        response.raise_for_status()
-        self.input_text.write_text(response.text, encoding="utf-8")
-
-    def _save_bin(self, path: Path, ids: List[int]):
-        arr = np.array(ids, dtype=self.dtype)
-        arr.tofile(path)
+    def _write_memap(self, path: Path, datesets, total_tokens):
+        arr = np.memmap(path, self.dtype, mode="w+", shape=(total_tokens,))
+        idx = 0
+        for example in datesets:
+            ids = np.array(example["ids"], dtype=self.dtype)
+            arr[idx : idx + len(ids)] = ids
+            idx += len(ids)
+        arr.flush()
 
     def _save_meta(self):
-        meta = {"vocab_size": self.tokenizer.n_vocab, "tokenizer_name": self.tokenizer_name}
+        meta = {"vocab_size": self.enc.n_vocab, "tokenizer_name": self.tokenizer_name}
         with self.meta_file.open("wb") as f:
             pickle.dump(meta, f)
 
 
 def main():
     config = PrepareConfig()
-    prepare = ShakespearePreparer(config)
+    prepare = TinyStoriesPreparer(config)
     prepare.prepare()
 
 
